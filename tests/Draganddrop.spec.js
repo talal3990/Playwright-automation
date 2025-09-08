2c26a1a237e49047c6d4bcffea570218c51fe77d
talalaftab <talalaftabkhan3@gmail.com>
2025-09-08 14:43:38 +0500

    draganddrop

--- tests/Draganddrop.spec.js
+++ tests/Draganddrop.spec.js
@@ -3,4 +3,25 @@ test('Drag and drop test',async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/drag_and_drop');
 
     await page.locator('#column-a').dragTo(await page.locator('#column-b'));
+
+    // const rome=await page.locator('#box6');
+    // const italy=await page.locator('#box106');
+    // Approach 1
+    // await rome.hover();
+    // await page.mouse.down();
+    // await italy.hover();
+    // await page.mouse.up();
+    // await page.waitForTimeout(5000);
+
+    //Approach 2
+    // await rome.dragTo(italy);
+
+// Washington to usa 
+// const washington=await page.locator('#box3');
+// const usa=await page.locator('#box103');
+// await washington.dragTo(usa);
+
+
+    await page.waitForTimeout(5000);
+
 })
